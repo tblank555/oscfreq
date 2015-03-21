457cb8078e0bc3d81925afce38282c7b1c7edf26
Travis Blankenship <tblank555@gmail.com>
2015-03-20 23:47:40 -0700

    made a pulsing chord

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 
 /**
- * @name untitled
+ * @name oscfreq
  */
 
-export function dsp(t) 
+var t;
+
+export function dsp(time) 
 {
-  return Math.sin(2 * Math.PI * t * 172.26);
+  t = time;
+  var note_a = 220;
+  var note_c = 261.63;
+  var note_e = 329.63;
+  
+  var mixed = triple_osc(note_a) * 0.4 + triple_osc(note_c) * 0.4 + triple_osc(note_e) * 0.4
+  var envelope = osc(1.0);
+  
+  return mixed * envelope;
+}
+
+function osc(freq)
+{
+  return Math.sin(2 * Math.PI * t * freq);
+}
+
+function triple_osc(freq)
+{
+  return osc(freq) * 0.8 + osc(freq * 2) * 0.4 + osc(freq * 4) * 0.35;
 }
