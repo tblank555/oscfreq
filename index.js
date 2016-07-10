3d4fc04374cc8169d47530ba300888dc842187a3
Travis Blankenship <tblank555@gmail.com>
2016-07-09 20:14:05 -0500

    added an envelope generator

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ export function dsp(time)
   var note_e = 329.63;
   
   var mixed = triple_osc(note_a) * 0.4 + triple_osc(note_c) * 0.4 + triple_osc(note_e) * 0.4
-  var envelope = osc(0.08);
+  var envelope = envelope_gen(0.001, 0.2);
   
   return mixed * envelope;
 }
@@ -26,4 +26,27 @@ function osc(freq)
 function triple_osc(freq)
 {
   return osc(freq) * 0.8 + osc(freq * 2) * 0.4 + osc(freq * 4) * 0.35;
+}
+
+function envelope_gen(attack, decay) 
+{
+  var totalLength = attack + decay;
+  
+  // currentTime is our cyclical time constant, essentially the x variable in our linear equations
+  var currentTime = t % totalLength;
+  
+  var y = 0.0;
+  if (currentTime <= attack) {
+    // We're in the attack phase
+    var slope = 1.0 / attack;
+    
+    y = slope * currentTime;
+  } else {
+    // We're in the decay phase
+    var slope = -1.0 / decay;
+    var y_intercept = attack / decay + 1.0;
+    y = slope * currentTime + y_intercept;
+  }
+  
+  return y;
 }
